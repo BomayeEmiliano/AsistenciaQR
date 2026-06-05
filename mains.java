06775f36746355439a338bcd8a8e427c12c7bdc7
BomayeEmiliano <emi_bore.r@hotmail.com>
2026-06-05 09:53:43 -0600

    Agrege codigo al metodo mains

---
+++ mains.java
@@ -0,0 +1,10 @@
+public class mains {
+    
+    public static void main(String[] args) {
+        usuario user=new usuario("Juanchis e Isra", "Novios");
+        user.getNombre();
+        System.out.println(user.getNombre());
+        user.getRol();
+        System.out.println(user.getRol());
+    }
+}

05d867f0a479b80e628d806f1d604e08a221de24
BomayeEmiliano <emi_bore.r@hotmail.com>
2026-06-10 09:38:41 -0600

    Agregue codigo a la clase docente

--- docente.java
+++ docente.java
@@ -1,5 +1,15 @@
-public class docente {
+public class docente extends usuario {
 
-    
-    
+    private int noEmpleado;
+
+    public docente(String nombre, String rol, int noEmpleado)
+    {
+        super(nombre, rol);
+        this.noEmpleado=noEmpleado;
+    }
+
+    public int getNoEmpleado()
+    {
+        return noEmpleado;
+    }
 }

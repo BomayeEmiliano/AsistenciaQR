a4619c4ed12ba9ccac44029887a3d0bb6767e5d4
BomayeEmiliano <emi_bore.r@hotmail.com>
2026-06-10 09:55:56 -0600

    Modifique MAINS instanciando a Alumno y Docente

--- mains.java
+++ mains.java
@@ -1,10 +1,23 @@
 public class mains {
     
     public static void main(String[] args) {
-        usuario user=new usuario("Juanchis e Isra", "Novios");
-        user.getNombre();
-        System.out.println(user.getNombre());
-        user.getRol();
-        System.out.println(user.getRol());
+        docente tichers=new docente("Cuitla", "maistro", 935);
+        tichers.getNoEmpleado();
+        System.out.println(tichers.getNoEmpleado());
+        tichers.getNombre();
+        System.out.println(tichers.getNombre());
+        tichers.getRol();
+        System.out.println(tichers.getRol());
+
+
+        alumno morrillo=new alumno("Juanillo", "Alumnito", "2530831",213564890);
+        morrillo.getMatricula();
+        System.out.println(morrillo.getMatricula());
+        morrillo.getNombre();
+        System.out.println(morrillo.getNombre());
+        morrillo.getRol();
+        System.out.println(morrillo.getRol());
+        morrillo.getQR();
+        System.out.println(morrillo.getQR());
     }
 }

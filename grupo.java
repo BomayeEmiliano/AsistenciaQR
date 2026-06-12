c965df47e21406c4ede1e4e6954bb0b18256eb50
BomayeEmiliano <emi_bore.r@hotmail.com>
2026-06-12 09:06:29 -0600

    Agregue codigo a la clase Grupo

--- grupo.java
+++ grupo.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class grupo {
-    
+    private String nombreGrupo;
+    private List<alumno> alumnos;
+
+    public grupo(String nombreGrupo)
+    {
+        this.nombreGrupo=nombreGrupo;
+        this.alumnos=new ArrayList<>();
+    }
+
+    public void agregarAlumnos(alumno a)
+    {
+        alumnos.add(a);
+    }
+
+    public List<alumno> getAlumno()
+    {
+        return alumnos;
+    }
 }

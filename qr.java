a6d12aaa583a7b2ae0fe91e2a6039ef726bfb46b
BomayeEmiliano <emi_bore.r@hotmail.com>
2026-06-12 09:15:40 -0600

    Agregue codigo a la clase QR

--- qr.java
+++ qr.java
@@ -1,3 +1,18 @@
 public class qr {
     
+    private String codigoQR;
+
+    public qr(String codigoQR)
+    {
+        this.codigoQR=codigoQR;
+    }
+
+    public String getQR(){
+        return codigoQR;
+    }
+
+    public boolean validar(String codigoIngresado)
+    {
+        return codigoQR.equals(codigoIngresado);
+    }
 }

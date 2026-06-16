0fc2cf07805faf3667d4716b37807fa50ee06c6c
BomayeEmiliano <emi_bore.r@hotmail.com>
2026-06-16 09:54:48 -0600

    Agrege codigo al archivo reporte

--- reporte.java
+++ reporte.java
@@ -1,3 +1,11 @@
+import java.util.List;
+
 public class reporte {
-    
+    public void generar(List<asistencia> asistencias){
+        System.out.println("=== REPORTE DE ASISTENCIA ===");
+        for (asistencia asis : asistencias) {
+            asis.mostrarAsistencia();
+        }
+
+    }
 }
